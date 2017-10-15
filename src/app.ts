838f4cdc42fe75c112729ab048a80bb6a13300db
Juan <cbenv@Juan-Mac.lan>
2017-10-14 20:57:19 -0700

    refactor: restructured app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,38 +5,31 @@ import * as nodemailer from 'nodemailer';
 
 import router from './server/router';
 
-class App 
-{
-  private setupVariables = function()
-  {
-    const config = require('../config.json');
-    this.host = config.node.host;
-    this.port = config.node.port;
-    this.smtp = config.mail.smtp;
-  }
-  private configureServer = function() 
-  {
-    const app = express();
-    const transport = nodemailer.createTransport(this.smtp);
+class App {
+  private config;
+  private http;
 
+  constructor() {
+    this.config = require('../config.json');
+  }
+
+  private configureServer() {
+    const app = express();
+    const transport = nodemailer.createTransport(this.config.mail.smtp);
     app.use(express.static(__dirname + '/client'));
     app.use(bodyParser.json());
     app.use('/', router(transport));
-
     this.http = http.createServer(app);
   }
-  public initialize = function() 
-  {
-    this.setupVariables();
+
+  public start() {
+    const port = this.config.node.port;
+    const host = this.config.node.host;
     this.configureServer();
-  }
-  public start = function()
-  {
-    this.http.listen(this.port, this.host);
-    console.log(`Application is running at ${this.host}:${this.port}`);
+    this.http.listen(port, host);
+    console.log(`Application is running at ${host}:${port}`);
   }
 }
 
 var app = new App();
-app.initialize();
 app.start();
